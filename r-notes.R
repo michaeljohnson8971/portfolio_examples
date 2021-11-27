1e04579b22343e1283cfee0be6f89fe53943b1f6
michaeljohnson8971 <michaeljohnson8971@gmail.com>
2021-11-27 16:07:47 -0500

    Create r-notes.R
    test notes add

---
+++ r-notes.R
@@ -0,0 +1,8 @@
+r uses 
+$ between a dataset and variable such as transport$buses 
+leaflet
+r adds and e to the end of census , e onthe end variabels 
+
+
+Project notes :
+  * epxlorring families with children/ totla families in school districts across the state of MA by census tract in suffolk county 
